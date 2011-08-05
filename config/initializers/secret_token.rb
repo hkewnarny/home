6600d9b315fa42b6149c7d2a47de13be8b4ad5b5
Henry <henry@ubuntu.ubuntu-domain>
2011-08-04 21:51:27 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Home::Application.config.secret_token = '840b803ed31277b5f67bc9d6257fa0a2b6a1c69908191eb6f1b7d8cd67dae29cfb84a6dac083195d4f1c3c3c248a5a4de88ede2092704d8f7749527bd6c024a0'
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe PagesController do
+
+  describe "GET 'index'" do
+    it "should be successful" do
+      get 'index'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'projects'" do
+    it "should be successful" do
+      get 'projects'
+      response.should be_success
+    end
+  end
+
+end
