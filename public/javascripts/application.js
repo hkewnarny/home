0ad144bdc47a03f252c8fd30a968fc54e4518c1f
Henry Kwan <hkwan@NLarimerosx6.cnet.cnwk>
2011-08-05 13:44:50 -0700

    Added in JS for scrolling sharing container

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-$(function() {
 
+
+$(function() {
+	var $sharingContainer = $("#social_container");
+	$(window).scroll(function() {
+		$sharingContainer.stop().animate(
+			{marginTop: $(window).scrollTop() + "px"},
+			"slow"
+			);
+	});
 });
